c614ccfdf676bc3b88eb9ede9bcdd1fa064a374e
Moahmmed Gomah <mohammedgomah125@gmail.com>
2025-02-25 20:38:09 +0200

    home - location - profile - splash

--- app/src/main/java/com/example/gomahrepoproject/splash/Splash3Fragment.kt
+++ app/src/main/java/com/example/gomahrepoproject/splash/Splash3Fragment.kt
@@ -13,7 +13,7 @@ class Splash3Fragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_splash4, container, false)
+        return inflater.inflate(R.layout.fragment_splash3, container, false)
     }
 
 
