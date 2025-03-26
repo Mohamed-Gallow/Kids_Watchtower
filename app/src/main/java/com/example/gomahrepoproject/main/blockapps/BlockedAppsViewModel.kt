dc28c18501f39e74cd26fe6301f36e1ebfc4b1db
Mohamed Gallow <medogallo7@gmail.com>
2025-03-26 15:21:22 +0200

    gallow appblock fragment beta

--- app/src/main/java/com/example/gomahrepoproject/main/blockapps/BlockedAppsViewModel.kt
+++ app/src/main/java/com/example/gomahrepoproject/main/blockapps/BlockedAppsViewModel.kt
@@ -8,10 +8,12 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.gomahrepoproject.main.data.repository.FirebaseRepository
 
 class BlockedAppsViewModel(application: Application) : AndroidViewModel(application) {
 
 
+    private val repository = FirebaseRepository()
     private val _blockedApps = MutableLiveData<List<String>>()
     val blockedApps: LiveData<List<String>> get() = _blockedApps
 
@@ -25,6 +27,11 @@ class BlockedAppsViewModel(application: Application) : AndroidViewModel(applicat
         }
     }
 
+    fun loadBlockedApps(userId: String, childId: String) {
+        repository.getBlockedApps(userId, childId) { apps ->
+            _blockedApps.postValue(apps)
+        }
+    }
 
     fun startMonitoringApps(onBlockedAppDetected: (String) -> Unit) {
         handler.post(object : Runnable {
